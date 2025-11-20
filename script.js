27394108f1a206c2914b72e492983a537a5b8a08
CHAIDARURROHMAN <chaidarrohman0@gmail.com>
2025-11-20 08:57:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Data Wisata Tuban yang Diperluas dan Stabil
+// Data Wisata Tuban yang Mengandung Koordinat Akurat
 const dataWisataTuban = {
     wisata: [
         { 
@@ -6,70 +6,70 @@ const dataWisataTuban = {
             deskripsi: "Hamparan pohon kelapa yang rindang, populer untuk spot foto dan bersantai.", 
             gambar: "PantaiKelapa.jpg",
             lokasi: "Desa Panyuran, Palang, Tuban",
-            query: "Pantai Kelapa Tuban" 
+            koordinat: "-6.896359464703487,112.08915280130618" // Koordinat
         },
         { 
             nama: "Air Terjun Nglirip", 
             deskripsi: "Air terjun berteras yang dikelilingi tebing karst, cocok untuk fotografi alam.", 
             gambar: "Nglirip.jpg",
             lokasi: "Desa Mulyoagung, Kecamatan Singgahan",
-            query: "Air Terjun Nglirip Tuban"
+            koordinat: "-7.018000,111.750500"
         },
         { 
             nama: "Goa Akbar", 
             deskripsi: "Gua alami yang luas dan berornamen stalaktit/stalagmit di pusat kota.", 
             gambar: "GoaAkbar.jpg",
             lokasi: "Jl. HR. Lukman, Semanding, Tuban",
-            query: "Goa Akbar Tuban"
+            koordinat: "-6.89531,112.05141"
         },
         { 
             nama: "Klenteng Kwan Sing Bio", 
             deskripsi: "Klenteng ikonik di tepi pantai, terkenal dengan patung dewa kura-kura.", 
             gambar: "Klenteng.jpg",
             lokasi: "Jl. Martadinata, Karangsari, Tuban",
-            query: "Klenteng Kwan Sing Bio Tuban"
+            koordinat: "-6.89269,112.04655"
         },
         { 
             nama: "Masjid Agung Tuban", 
             deskripsi: "Masjid bersejarah dengan arsitektur perpaduan Jawa dan Timur Tengah.", 
             gambar: "MasjidAgung.jpg",
             lokasi: "Jl. Bonang, Kutorejo, Tuban",
-            query: "Masjid Agung Tuban"
+            koordinat: "-6.89063,112.05510"
         },
         {
             nama: "Sendang Asmoro",
             deskripsi: "Kolam alami dengan air jernih dan suasana hutan yang sejuk. Populer untuk berenang.",
             gambar: "SendangAsmoro.jpg",
             lokasi: "Desa Ngino, Kecamatan Semanding",
-            query: "Sendang Asmoro Tuban"
+            koordinat: "-6.90820,111.95600"
         },
         {
             nama: "Wisata Mangrove Center",
             deskripsi: "Kawasan hutan bakau yang dilengkapi jembatan kayu untuk *tracking* dan edukasi lingkungan.",
             gambar: "MangroveCenter.jpg",
             lokasi: "Jenu, Tuban",
-            query: "Wisata Mangrove Center Tuban"
+            koordinat: "-6.88510,112.00030"
         },
         {
             nama: "Air Terjun Kedungjambe",
             deskripsi: "Air terjun tersembunyi dengan kolam yang tenang, membutuhkan *trekking* ringan.",
             gambar: "Kedungjambe.jpg",
             lokasi: "Desa Boto, Kecamatan Semanding",
-            query: "Air Terjun Kedungjambe Tuban"
+            koordinat: "-6.95300,112.08300"
         },
         {
             nama: "Makam Sunan Bonang",
             deskripsi: "Kompleks makam salah satu Wali Songo, menjadi pusat ziarah utama di Tuban.",
             gambar: "SunanBonang.jpg",
             lokasi: "Kutorejo, Tuban",
-            query: "Makam Sunan Bonang Tuban"
+            koordinat: "-6.89030,112.05450"
         },
         {
             nama: "Pantai Sowan",
             deskripsi: "Pantai dengan pepohonan pinus dan perahu nelayan, menawarkan pemandangan matahari terbenam.",
             gambar: "PantaiSowan.jpg",
             lokasi: "Desa Bogorejo, Bancar",
-            query: "Pantai Sowan Tuban"
+            koordinat: "-6.75050,111.60300"
         }
     ]
 };
@@ -86,20 +86,20 @@ function displayResults() {
             const card = document.createElement('div');
             card.className = 'result-card';
             
-            // SOLUSI FINAL UNTUK URL: Menggunakan format kanonikal Google Maps API
-            // Format ini 100% aman dan tidak rentan terhadap short-link/proxy error.
-            const mapsUrl = "https://maps.app.goo.gl/KlentengKwanSingBio7" + encodeURIComponent(item.query);
+            // SOLUSI TERAKHIR: Menggunakan format koordinat langsung (paling stabil)
+            // Ini akan memicu aplikasi/browser untuk mencari lokasi koordinat tersebut.
+            const mapsUrl = "https://maps.app.goo.gl/KlentengKwanSingBio9" + item.koordinat;
             
             card.innerHTML = `
                 <div class="card-images">
-                    <img src="images/${item.gambar}" alt="${item.nama}" onerror="this.onerror=null;this.src='http://googleusercontent.com/28';">
+                    <img src="images/${item.gambar}" alt="${item.nama}" onerror="this.onerror=null;this.src='http://googleusercontent.com/29';">
                 </div>
                 <div class="card-content">
                     <h3>${item.nama}</h3>
                     <p>${item.deskripsi}</p>
                     <p class="location-text">📍 ${item.lokasi}</p>
                     <a href="${mapsUrl}" target="_blank" class="map-link">
-                        Lihat Lokasi di Google Maps
+                        Lihat Lokasi Tepat
                     </a>
                 </div>
             `;
